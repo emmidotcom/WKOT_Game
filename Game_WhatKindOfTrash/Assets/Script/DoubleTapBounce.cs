e6c61d61bbded999dc3509ab5a997cba5b5ec476
HelenaHoger <81622071+HelenaHoger@users.noreply.github.com>
2021-05-19 12:12:10 +0200

    Aktueller Stand Helena
    Müll Package eingeladen. Tags verteilt.

--- Game_WhatKindOfTrash/Assets/Script/DoubleTapBounce.cs
+++ Game_WhatKindOfTrash/Assets/Script/DoubleTapBounce.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DoubleTapBounce : MonoBehaviour
 {
+
     public float tapForce = 10;                             //Stärke des Bounce -> in Unity selbst variierbar
     public float tiltSmooth = 2;                            //Smoothnes des Bounce -> in Unity selbst variierbar
     public Vector3 startPos;
@@ -20,15 +21,14 @@ public class DoubleTapBounce : MonoBehaviour
 
 
 
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))                    //Wenn Maus Gedrückt dann:
-        {
-            transform.rotation = forwardRotation;           //Jump-Rotation tritt ein
-            rigidbody.velocity = Vector3.zero;
-            rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);       //OBJ jumped/bounced mit TapForce (Stärke des Bounce)
-        }
 
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+
+        transform.rotation = forwardRotation;           //Jump-Rotation tritt ein
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);       //OBJ jumped/bounced mit TapForce (Stärke des Bounce)
         transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);        //Fall-Rotation nur während OBJ angeklickt wird
     }
 }
---
+++ Game_WhatKindOfTrash/Assets/Script/GoodPoints.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  //WICHTIG!!! Script muss auf UnityEngine.UI zugreifen können sonst versteht das Script die Befehle nicht
+
+public class GoodPoints : MonoBehaviour
+{
+    public static GoodPoints instance;    //Script wird für andere Sripte verwendbar
+
+    public Text scoreText;                  //Welcher Text ist gemeint
+
+    int score =  0;                          //Score am Anfang 0
+
+    private void Awake()                    //Script wird für andere Sripte verwendbar             
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        scoreText.text = "" + score.ToString();   //Angezeigter Text = "x und ScoreKette (addierte Zahlen Kette)
+    }
+
+    public void AddGoodPoint()                          //Eine Score Variante +15  ->  richtig zugeordnet
+    {
+        score += 15;
+        scoreText.text = "+" + score.ToString();
+    }
+
+}
