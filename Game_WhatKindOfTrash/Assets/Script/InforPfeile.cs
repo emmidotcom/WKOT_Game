9b6e91be11fa257cbd4244f02f34960677ace83c
HelenaHoger <81622071+HelenaHoger@users.noreply.github.com>
2021-06-29 11:09:49 +0200

    neue schrift usw

---
+++ Game_WhatKindOfTrash/Assets/Script/InforPfeile.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPfeile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] InfoElemente;
+    public GameObject PfeilL;
+    public GameObject PfeilR;
+
+
+
+
